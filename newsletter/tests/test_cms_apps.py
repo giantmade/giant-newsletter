f29218f288589453448f62d3013b2b8d3400de19
Will-Hoey <will.hoey@hotmail.co.uk>
2020-06-12 10:12:23 +0100

    Allow cms_apps test to run

--- newsletter/tests/test_cms_apps.py
+++ newsletter/tests/test_cms_apps.py
@@ -1,8 +1,6 @@
-import pytest
 from newsletter.cms_apps import NewsletterApp
 
 
-@pytest.importorskip("django.settings.INSTALLED_APPS")
 class TestNewsletterApp:
     """
     Test case for the EventsApp
@@ -13,4 +11,4 @@ class TestNewsletterApp:
         Test get_urls method on the ContactApp class
         """
 
-        assert NewsletterApp().get_urls() == ["contact.urls"]
+        assert NewsletterApp().get_urls() == ["newsletter.urls"]
