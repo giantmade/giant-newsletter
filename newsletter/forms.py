7fb6ea0ad4451ab65c5fe52d7ca7ed38568802e1
Will-Hoey <will.hoey@hotmail.co.uk>
2020-06-23 15:37:44 +0100

    Add custom fields to form

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.conf import settings
 
 from . import models
 
@@ -7,7 +8,30 @@ class SubscriptionForm(ModelForm):
     """
     Modelform for subscription model
     """
+    required_fields = getattr(settings, "NEWSLETTER_FORM_REQUIRED_FIELDS", [])
+    field_placeholders = getattr(settings, "NEWSLETTER_FORM_FIELD_PLACEHOLDERS", {})
 
     class Meta:
         model = models.Subscription
-        fields = ["first_name", "last_name", "email"]
+        fields = getattr(
+            settings,
+            "NEWSLETTER_FORM_FIELDS",
+            ["first_name", "last_name", "email"],
+        )
+        labels = getattr(settings, "NEWSLETTER_FORM_LABELS", None)
+        widgets = getattr(settings, "NEWSLETTER_FORM_WIDGETS", None)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Override the init method to set the customised required fields
+        """
+        super().__init__(*args, **kwargs)
+
+        # Update the required fields
+        for field in self.required_fields:
+            self.fields[field].required = True
+
+        # Update field placeholder
+        for field, placeholder in self.field_placeholders.items():
+            field = self.fields.get(field)
+            field.widget.attrs["placeholder"] = placeholder
